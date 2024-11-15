25362f02434aef53a45955f980e3a483e9fca2ef
addittidas <aditidas.coding@gmail.com>
2024-11-15 21:16:24 +0530

    Added

--- Math/Q9_PalindromeNumber.java
+++ Math/Q9_PalindromeNumber.java
@@ -1,8 +1,5 @@
-// 7. Reverse Integer
-/* Given a signed 32-bit integer x, return x with its digits reversed.
-If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
-
-Assume the environment does not allow you to store 64-bit integers (signed or unsigned). */
+// 9. Palindrome Number
+/* Given an integer x, return true if x is a palindrome, and false otherwise. */
 
 class Q9_PalindromeNumber {
     public boolean isPalindrome(int x) {
