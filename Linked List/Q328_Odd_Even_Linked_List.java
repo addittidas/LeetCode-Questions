9099aa28390d54c6405da26c3bfacbc961d5c912
addittidas <aditidas.coding@gmail.com>
2024-11-16 20:06:46 +0530

    Added

---
+++ Linked List/Q328_Odd_Even_Linked_List.java
@@ -0,0 +1,39 @@
+// 328. Odd Even Linked List
+/* Given the head of a singly linked list, group all the nodes with odd indices together
+followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity. */
+
+class Q328_Odd_Even_Linked_List {
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode temp = even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = temp;
+        return head;
+    }
+}
