28a4356b13363827bb9b684b24736b7b364e3319
addittidas <aditidas.coding@gmail.com>
2025-01-31 00:32:46 +0530

    Added

---
+++ Q104_Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,28 @@
+// 104. Maximum Depth of Binary Tree
+/* Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node. */
+
+public class Q104_Maximum_Depth_of_Binary_Tree {
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    // Recursive Approach
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
